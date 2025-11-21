ba1e9e8fe9cdff04aeaef1589bb4352a8ee49709
Federico Moretti <hello@federicomoretti.it>
2025-11-21 14:35:21 +0100

    fix: change agent name since 'operator' seems to be a reserved word

--- agents/agent/agent.py
+++ agents/agent/agent.py
@@ -2,17 +2,17 @@ from google.adk.agents import Agent
 from google.adk.models.lite_llm import LiteLlm
 from google.genai import types
 
-from .sub_agents.nba import nba
+from .sub_agents.nba.agent import nba
 
-operator = Agent(
+root_agent = Agent(
     model = LiteLlm(
         model = "ollama_chat/gemma3:latest",
         api_base = "http://localhost:11434",
     ),
-    name = "operator",
+    name = "agent",
     description = "",
     instruction = """
-        You are an experienced customer service operator that replies gently to the customer.
+        You are an experienced customer service agent that replies gently to the customer.
     """,
     sub_agents = [ nba ],
     tools = []
