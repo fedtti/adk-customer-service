b3bfb38d0ce6aaf4baec9e1f3826bb068119463a
Federico Moretti <hello@federicomoretti.it>
2025-12-09 13:58:26 +0100

    chore: add try/except, remove LiteLLM support

--- agents/supervisor/agent.py
+++ agents/supervisor/agent.py
@@ -1,6 +1,4 @@
 from google.adk.agents import Agent
-# from google.adk.runners import Runner
-# from google.adk.sessions import InMemorySessionService
 from google.genai import types
 
 print("✅ Libraries imported.")
@@ -11,32 +9,37 @@ from .sub_agents.audit import agent as audit
 print("✅ Agents imported.")
 
 """ """
-supervisor = Agent(
-    model = "gemini-3-pro-preview",
-    name = "supervisor",
-    description = "Supervise agents’ activities",
-    instruction = f"""
-        You are an experienced customer service supervisor.
-        Your role is to supervise agents’ activities.
+supervisor = None
+try:
+    supervisor = Agent(
+        model = "gemini-3-pro-preview",
+        name = "supervisor",
+        description = "Supervise agents’ activities",
+        instruction = f"""
+            You are an experienced customer service supervisor.
+            Your role is to supervise agents’ activities.
 
-        You have two specialized sub-agents:
-        1. '{agent.agent.name}': Handles users’ questions. Delegate to it if the user asks a question.
-        2. '{audit.audit.name}': Handles agent’s answers. Delegate to it after '{agent.agent.name}' answered the user question.
+            Handle greetings and farewell providing polite messages.
 
-        You also have a tool:
-        - '': 
+            You have two specialized sub-agents:
+            1. '{agent.agent.name}': Handles users’ questions. Delegate to it if the user asks a question.
+            2. '{audit.audit.name}': Handles agent’s answers. Delegate to it after '{agent.agent.name}' answered the user question.
 
-        For anything else, respond appropriately or state you cannot handle it.
-    """,
-    sub_agents = [
-        agent,
-        audit
-    ],
-    tools = [
+            You also have a tool:
+            - '': 
+
+            For anything else, respond appropriately or state you cannot handle it.
+        """,
+        sub_agents = [
+            agent,
+            audit
+        ],
+        tools = [
       
-    ]
-)
+        ]
+    )
+    print(f"✅ Agent '{supervisor.name}' created using model '{supervisor.model}' with sub-agents: {[]}, and tools: {[]}.")
+except Exception as error:
+    print(f"❌ .")
 
-print(f"✅ Agent '{supervisor.name}' created using model '{supervisor.model}' with sub-agents: {[]}, and tools: {[]}.")
-
-root_agent = supervisor # Export for ADK.
+root_agent = supervisor # Export for Google Agent Development Kit (ADK).
